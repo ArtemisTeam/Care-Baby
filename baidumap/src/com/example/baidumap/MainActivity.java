d27a7e265e088d4ce4f05310dde287d7292b28e7
halfcrazy <hackzhuyan@gmail.com>
2014-02-17 13:58:05 +0800

    添加安全范围，画圈未完成。

--- baidumap/src/com/example/baidumap/MainActivity.java
+++ baidumap/src/com/example/baidumap/MainActivity.java
@@ -6,6 +6,11 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.os.Handler;
 import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.avos.avoscloud.AVAnalytics;
 import com.avos.avoscloud.AVException;
@@ -13,19 +18,37 @@ import com.avos.avoscloud.AVOSCloud;
 import com.avos.avoscloud.AVObject;
 import com.avos.avoscloud.AVQuery;
 import com.avos.avoscloud.FindCallback;
-import com.avos.avoscloud.GetCallback;
 import com.baidu.mapapi.BMapManager;
+import com.baidu.mapapi.map.Geometry;
+import com.baidu.mapapi.map.Graphic;
+import com.baidu.mapapi.map.GraphicsOverlay;
 import com.baidu.mapapi.map.LocationData;
+import com.baidu.mapapi.map.MKMapTouchListener;
 import com.baidu.mapapi.map.MapView;
 import com.baidu.mapapi.map.MyLocationOverlay;
+import com.baidu.mapapi.map.Symbol;
+import com.baidu.mapapi.map.Symbol.Color;
+import com.baidu.mapapi.map.Symbol.Stroke;
+import com.baidu.mapapi.search.PlaceCaterActivity;
+import com.baidu.mapapi.utils.DistanceUtil;
 import com.baidu.platform.comapi.basestruct.GeoPoint;
 
-public class MainActivity extends Activity{
+public class MainActivity extends Activity implements OnClickListener{
 	BMapManager mBMapMan = null;
 	MapView mMapView = null;
+	Button current_loc=null;
+	Button trajectory=null;
+	Button set_range=null;
+	EditText edt=null;
 	LocationData locData=null;
 	MyLocationOverlay myLocationOverlay=null;
 	String username="211";
+	int set_distance;
+	int tar_la;
+	int tar_lo;
+	boolean isFollowTrail=false;
+	boolean isShowTrail=false;
+	boolean isSetRange=false;
 	@Override
 	public void onCreate(Bundle savedInstanceState){
 		super.onCreate(savedInstanceState);
@@ -34,25 +57,60 @@ public class MainActivity extends Activity{
 		AVAnalytics.trackAppOpened(getIntent());
 		mBMapMan=new BMapManager(getApplication());
 		mBMapMan.init("8GX4jfnPmYXXeTwXP3QcccAy", null);  
+
 		//注意：请在试用setContentView前初始化BMapManager对象，否则会报错
 		setContentView(R.layout.activity_main);
+		initView();
+		initData();
+	}
 
+	public void initView()
+	{
 		mMapView=(MapView)findViewById(R.id.bmapsView);
+		current_loc=(Button)findViewById(R.id.button1);
+		current_loc.setOnClickListener(this);
+		trajectory=(Button)findViewById(R.id.button2);
+		trajectory.setOnClickListener(this);
+		set_range=(Button)findViewById(R.id.button3);
+		set_range.setOnClickListener(this);
+		edt=(EditText)findViewById(R.id.editText1);
+	}
+	public void initData()
+	{
+		MKMapTouchListener mapTouchListener = new MKMapTouchListener(){
+			@Override
+			public void onMapClick(GeoPoint point) {
+				//在此处理地图单击事件
+			}
 
+			@Override
+			public void onMapDoubleClick(GeoPoint point) {
+				//在此处理地图双击事件
+			}
+
+			@Override
+			public void onMapLongClick(GeoPoint point) {
+				//在此处理地图长按事件 
+				tar_la=point.getLatitudeE6();
+				tar_lo=point.getLongitudeE6();
+			}
+		};
 		myLocationOverlay = new MyLocationOverlay(mMapView);
 		locData = new LocationData();
 		//手动将位置源置为天安门，在实际应用中，请使用百度定位SDK获取位置信息，要在SDK中显示一个位置，需要使用百度经纬度坐标（bd09ll）
-		locData.latitude = 39.945;
-		locData.longitude = 116.404;
+		//		locData.latitude = 39.945;
+		//		locData.longitude = 116.404;
 		//locData.direction = 2.0f;
-		myLocationOverlay.setData(locData);
+		//		myLocationOverlay.setData(locData);
 		mMapView.getOverlays().add(myLocationOverlay);
 		mMapView.refresh();
 		mMapView.setSatellite(true);  
 		mMapView.getController().setZoom(19);
-		mMapView.getController().animateTo(new GeoPoint((int)(locData.latitude*1e6),
-				(int)(locData.longitude* 1e6)));
-		handler.postDelayed(runnable,5000); 
+		//		GeoPoint p1=new GeoPoint((int)(locData.latitude*1e6),
+		//				(int)(locData.longitude* 1e6));
+		//		mMapView.getController().animateTo(p1);
+		//		handler.postDelayed(runnable,5000); 
+
 		////        	mMapView.setBuiltInZoomControls(true);
 		//        	mMapView.setSatellite(true);  
 		//        	//设置启用内置的缩放控件
@@ -76,18 +134,34 @@ public class MainActivity extends Activity{
 					// TODO Auto-generated method stub
 					if(arg1==null){
 						if(arg0!=null){
-						Log.i("state", arg0.size()+"");
-						AVObject point;
-						point=arg0.get(0);
-						locData.latitude=point.getDouble("Latitude");
-						locData.longitude=point.getDouble("Longtitude");
-						Log.i("loc", "latitude "+locData.latitude);
-						Log.i("loc", "longtitude "+locData.longitude);
-						myLocationOverlay.setData(locData);
-						mMapView.refresh();
-						mMapView.getController().animateTo(new GeoPoint((int)(locData.latitude*1e6),
-								(int)(locData.longitude* 1e6)));
-						Log.i("state", "reanimate");
+							Log.i("state", arg0.size()+"");
+							AVObject point;
+							point=arg0.get(0);
+							if(locData.latitude==point.getDouble("Latitude")
+									&&locData.longitude==point.getDouble("Longtitude")){
+								return;
+							}
+							locData.latitude=point.getDouble("Latitude");
+							locData.longitude=point.getDouble("Longtitude");
+							Log.i("loc", "latitude "+locData.latitude);
+							Log.i("loc", "longtitude "+locData.longitude);
+							if(isFollowTrail==true){
+								myLocationOverlay.setData(locData);
+								mMapView.refresh();
+								GeoPoint p1=new GeoPoint((int)(locData.latitude*1e6),
+										(int)(locData.longitude* 1e6));
+								mMapView.getController().animateTo(p1);
+								Log.i("state", "reanimate");
+							}else if(isSetRange==true){
+								GeoPoint p1=new GeoPoint((int)(locData.latitude*1e6),
+										(int)(locData.longitude* 1e6));
+								GeoPoint p2=new GeoPoint(tar_la, tar_lo);
+								double distance = DistanceUtil.getDistance(p1, p2);
+								if(distance>set_distance){
+									Toast.makeText(getApplicationContext(), "孩子超出安全范围！",
+											Toast.LENGTH_SHORT).show();
+								}
+							}
 						}else{
 							Log.i("state","query for nothing");
 						}
@@ -126,6 +200,48 @@ public class MainActivity extends Activity{
 			mBMapMan.start();
 		}
 		super.onResume();
+	}
+
+	@Override
+	public void onClick(View v) {
+		// TODO Auto-generated method stub
+		switch (v.getId()) {
+		case R.id.button1:
+			FollowTrail();
+			break;
+		case R.id.button2:
+			ShowTrail();
+			break;
+		case R.id.button3:
+			SetRange();
+		}
+	}
+
+	public void FollowTrail(){
+		if(isFollowTrail==false){
+			isFollowTrail=true;
+		}else{
+			isFollowTrail=false;
+		}
+	}
+
+	public void ShowTrail(){
+
+	}
+
+	public void SetRange(){
+		if(isSetRange==false){
+			isSetRange=true;	
+			set_distance=Integer.parseInt(edt.getText().toString().trim());
+		}else{
+			isSetRange=false;
+			edt.setText(null);
+		}
+		//画圈待添加
+//		GeoPoint p=new GeoPoint(tar_la, tar_lo);
+//		Geometry palaceGeometry = new Geometry();
+//		palaceGeometry.setCircle(p, set_distance);
+		
 	}
 
 } 
